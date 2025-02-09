31a059004f01873f53038c6309d78fe99bcdbb36
Julia Teymourian <julia.teymourian@autodesk.com>
2025-02-08 22:25:12 -0500

    Fix React version conflicts and reinstall dependencies

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', background: 'linear-gradient(to right, #ff9a9e, #fad0c4)' }}>
+      <h1 style={{ fontSize: '3rem', color: 'white' }}>Welcome to My 3D Cartoon Gallery!</h1>
+    </div>
+  );
+}
+
